63451fd0a9f4886f24bcaceb39e17f1bee6b9821
wei simeng <31880831+ComnetGi@users.noreply.github.com>
2022-07-07 10:54:09 +0900

    Update and rename credit.js to result.js

--- result/result.js
+++ result/result.js
@@ -1,5 +1,4 @@
 window.onload = function () {
     const urlParams = new URLSearchParams(window.location.search);
-    console.log(urlParams.get('rst'));
     document.getElementById("rst1").innerHTML = urlParams.get('rst');
 }
