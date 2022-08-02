4da84b60ae54c0f101a6fa2f58a95cb0e0a24714
ComnetGi <s.gi@comnet-network.co.jp>
2022-08-02 10:10:45 +0900

    payET

--- result/result.js
+++ result/result.js
@@ -1,4 +1,5 @@
 window.onload = function () {
     const urlParams = new URLSearchParams(window.location.search);
     document.getElementById("rst").innerHTML = urlParams.get('rst');
+    document.getElementById("その他").innerHTML = urlParams.get('その他');
 }
