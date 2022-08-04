2ff8def06ecc1f59eedd82e29be7921764ed7bcd
ComnetGi <s.gi@comnet-network.co.jp>
2022-08-04 13:59:36 +0900

    u

--- result/result.js
+++ result/result.js
@@ -1,9 +1,10 @@
 window.onload = function () {
     const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.get('rst') = 1) {
-        document.getElementById("result").innerHTML = "決済成功";
-        //document.getElementById("userIds").innerHTML = urlParams.get('その他').split(',');
-    } else {
-        document.getElementById("result").innerHTML = "決済失敗";
-    }
+    // if (urlParams.get('rst') = 1) {
+    //     document.getElementById("result").innerHTML = "決済成功";
+    //     //document.getElementById("userIds").innerHTML = urlParams.get('その他').split(',');
+    // } else {
+    //     document.getElementById("result").innerHTML = "決済失敗";
+    // }
+    document.getElementById("result").innerHTML = urlParams.get('rst') ;
 }
