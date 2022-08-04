4faf75dabf9924d10ce64c24a99d710a9d357e1f
ComnetGi <s.gi@comnet-network.co.jp>
2022-08-04 13:57:03 +0900

    up

--- result/result.js
+++ result/result.js
@@ -1,10 +1,9 @@
 window.onload = function () {
     const urlParams = new URLSearchParams(window.location.search);
     if (urlParams.get('rst') = 1) {
-        document.getElementById("rst").innerHTML = "決済成功";
-        document.getElementById("userIds").innerHTML = urlParams.get('その他').split(',');
+        document.getElementById("result").innerHTML = "決済成功";
+        //document.getElementById("userIds").innerHTML = urlParams.get('その他').split(',');
     } else {
-        document.getElementById("rst").innerHTML = "決済失敗";
+        document.getElementById("result").innerHTML = "決済失敗";
     }
-
 }
