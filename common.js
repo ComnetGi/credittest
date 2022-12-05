b4d9286bec21a6840c5d3195ced38ca5468f8f4d
ComnetGi <s.gi@comnet-network.co.jp>
2022-12-05 11:58:15 +0900

    2

--- common.js
+++ common.js
@@ -1,51 +1,22 @@
-var priceTaxExcluded = 0;
-var tax = 0;
-var userIds = "";
+var totalprice = 0;
 
 function calc_price_total() {
-    num = 0;
-    b = document.querySelectorAll(".form-check-input");
-    userIdsTemp = "";
-    for (var i = 1; i < b.length; i++) {
-        if (b[i].checked) {
-            userIdsTemp += b[i].id + ",";
-            num += 1;
-            if (num < 6) {
-                document.getElementById("unitPrice").innerHTML = "0";
-            } else {
-                document.getElementById("unitPrice").innerHTML = "300";
-                priceTaxExcluded = num * b[i].value;
-                tax = priceTaxExcluded * 0.1;
-            }
-        }
-    }
-    userIds = userIdsTemp;
-    console.log(userIds);
-    document.getElementById("licenseNum").innerHTML = num;
-    document.getElementById("totalPrice").innerHTML = priceTaxExcluded.toLocaleString() + "  円 (税抜き)";
+    
+    unitprice = document.getElementById("lbUnitPrice").innerHTML;
+    licensenum = document.getElementById("tbxLicenseNum").value;
+
+
+
+    document.getElementById("lbLicenseNum").innerHTML = licensenum;
+    totalprice = licensenum * unitprice;
+    document.getElementById("lbTotalPrice").innerHTML = totalprice.toLocaleString() + "  円 (税抜き)";
 
 }
 
 function payBtn() {
-    document.getElementById("am").value = priceTaxExcluded;
-    document.getElementById("tx").value = tax;
-    document.getElementById("userIds").value = userIds;
-
+    document.getElementById("am").value = totalprice;
+    document.getElementById("tx").value = totalprice * 0.1;
+    document.getElementById("acam").value = totalprice;
+    document.getElementById("actx").value = totalprice * 0.1;
 }
 
-function payBtn2() {
-    document.getElementById("am2").value = priceTaxExcluded;
-    document.getElementById("tx2").value = tax;
-    document.getElementById("userIds2").value = userIds;
-
-}
-
-function checkBtn() {
-    if (document.getElementById("flexSwitchCheckChecked").checked) {
-        document.getElementById("firstPay").style.display = "none";
-        document.getElementById("notFirstPay").style.display = "block";
-    } else {
-        document.getElementById("firstPay").style.display = "block";
-        document.getElementById("notFirstPay").style.display = "none";
-    }
-}
