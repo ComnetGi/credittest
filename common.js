23170503998b5e9ed87ddd0321cfb28ef573f786
ComnetGi <s.gi@comnet-network.co.jp>
2022-12-05 15:59:42 +0900

    2

--- common.js
+++ common.js
@@ -16,20 +16,16 @@ function calc_price_total() {
 function payBtn() {
 
     var today = new Date();
-    var billingDay = today.getDate();
     today.setDate(today.getDate() + 7);
     var year = today.getFullYear();
     var month = today.getMonth() + 1;
     var day = today.getDate();
     var billingStartDay =  year + "/" + month + "/" + day;
 
-
-
     document.getElementById("am").value = totalprice;
     document.getElementById("tx").value = totalprice * 0.1;
     document.getElementById("acam").value = totalprice;
     document.getElementById("actx").value = totalprice * 0.1;
-    document.getElementById("ac1").value = billingDay;
     document.getElementById("ac4").value = billingStartDay;
 
 }
